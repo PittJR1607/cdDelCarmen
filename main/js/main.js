d52b5a2ec28d7f761abe4c4b41a85bbfd03d5178
= <=>
2023-05-03 14:21:04 -0600

    Finished courses and header

--- main/js/main.js
+++ main/js/main.js
@@ -6,19 +6,35 @@ class Card {
     constructor(){
         this.card = document.createElement('div')
         this.cardText = document.createElement('p')
-        this.cardProfessor = document.createElement('p')
-        this.cardDescription = document.createElement('p')
 
+        this.cardDescription = document.createElement('p')
+        this.cardImgDiv = document.createElement('div')
+        this.cardButton = document.createElement('button')
     }
 
     buildCard(course){
-        this.card.appendChild(this.cardText)
-        this.card.appendChild(this.cardProfessor)
-        this.card.appendChild(this.cardDescription)
 
-        this.cardText.textContent = course.courseName
-        this.cardProfessor.textContent = course.professor
+        this.card.appendChild(this.cardImgDiv)
+        this.card.appendChild(this.cardText)
+        this.card.appendChild(this.cardDescription)
+        this.card.appendChild(this.cardButton)
+        this.card.className = "card"
+
+        this.cardImgDiv.style.backgroundImage = `url(${course.imgUrl})`
+        this.cardImgDiv.style.backgroundSize = "cover"
+        this.cardImgDiv.style.backgroundRepeat = "no-repeat"
+        this.cardImgDiv.style.backgroundPosition = "bottom"
+        this.cardImgDiv.style.maxWidth = "100%"
+        this.cardImgDiv.style.height = "20rem"
+
+        this.cardText.textContent = "Actividad: " + course.courseName
+        this.cardText.className = "card-text"
+
         this.cardDescription.textContent = course.description 
+        this.cardDescription.className = "card-description"
+        this.cardButton.className = "card-button"
+        this.cardButton.textContent = "Ver detalles"
+        
     }
 
 }
